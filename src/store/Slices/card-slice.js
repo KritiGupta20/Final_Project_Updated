30a5afd8fc27cdeee2012830f7b1f5baeb73192b
KritiGupta20 <kritigupta2599@gmail.com>
2021-12-01 12:28:20 +0530

    third commit

--- src/store/Slices/card-slice.js
+++ src/store/Slices/card-slice.js
@@ -1,21 +1,21 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const cardSlice = createSlice({
-	name: "card",
-	initialState: {
-		cardNumber: "",
-		expiry: "",
-		cvv: "",
-		cardHolderName: "",
-	},
-	reducers: {
-		updateCard(state, action) {
-			console.log(action.payload);
-			const { key, value } = action.payload;
+  name: "card",
+  initialState: {
+    cardNumber: "",
+    expiry: "",
+    cvv: "",
+    cardHolderName: "",
+  },
+  reducers: {
+    updateCard(state, action) {
+      console.log(action.payload);
+      const { key, value } = action.payload;
 
-			state[key] = value;
-		},
-	},
+      state[key] = value;
+    },
+  },
 });
 
 export const cardActions = cardSlice.actions;
